24ce865b3203bf3c4e3fbf070b7d76a8e19280b6
Ruben5496 <rubenbarca1@hotmail.com>
2015-09-25 15:51:26 -0600

    Registred Machine

--- application.py
+++ application.py
@@ -1,3 +1,31 @@
-# Aqui escribe tu codigo
+"""
+In this Application We create a register machine 
+"""
+print """
+"""
+import os
+import sys 
+TOTAL= 0.0
+def EXIT():#Here we created an function that allow to the user an option to get out
+	print "Thanks for Visit Us"
+	raw_input("Press Enter for Continue n.n")
+	sys.exit()
 
-print "Hello World"
+def Menu():#Here we menu that shows the options that user can select!
+	while True: 
+		os.system("cls")
+		print "Welcome, please select an option\n"
+		print "#1 Add an item "
+		print "#2 Sell Articles"
+		print "#3 Exit"
+		DATA=raw_input("Insert an Option\n")
+		"""if DATA =="1":
+			TOTAL =  
+		elif DATA == "2":
+		"""	
+		if DATA == "3":
+			EXIT()
+		else:
+			print "No entiendo la instruccion"
+			raw_input("press enter to continue")
+Menu()
