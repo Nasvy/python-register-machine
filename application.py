69605027683949fd44710058613e7ecf75ea03ec
Ruben5496 <rubenbarca1@hotmail.com>
2015-09-28 16:23:55 -0600

    first 3 stories

--- application.py
+++ application.py
@@ -11,41 +11,86 @@ print """
 import os
 import sys 
 TOTAL= 0.0
-SELL = {
-	"PRICE" : []
-}
+SELL = {}
+def DISCOUNT():
+	CARD = raw_input("Do you have a gold or silver card?\n")
+	CARD = CARD.lower()
+	if CARD == "gold" or CARD == "gold card":
+		REDUCTION =	TOTAL * 0.5
+		TOTAL = TOTAL - REDUCTION
+		return TOTAL
+	elif CARD == "silver" or CARD == "silver card":
+		REDUCTION = TOTAL * 0.2
+		TOTAL = TOTAL - REDUCTION
+		return TOTAL	
+#here the cashier can insert another articles
 def QUESTION():
-	QUEST = raw_input("Do you want to Insert another article? Y/N\n")
+	QUEST = raw_input("Do you want to Insert another article? Y/N or you're Done?\n")
 	QUEST = QUEST.lower()
 	if QUEST == "y" or QUEST == "yes":
 		INPUTS()
 	elif QUEST == "n" or QUEST == "no":
 		MENU()
+	elif QUEST == "Done":
+		pass
 	else:
 		print "Insert a valid option"
 		QUESTION()
-def INPUTS():
-	ARTICLE= raw_input("Insert first article\n")
-	QUANTITY = raw_input("Insert Quantity article\n")
-	PRICE = float(raw_input("Insert the price of the article\n"))
-	QUESTION()
-def EXIT():#Here we create a function that allow to the user an option to get out
+#Here the Cashier insert the articles
+def ARTICLE():
+	ART = True
+	while True:
+		ARTICLE = raw_input("Insert first article\n")
+		try:
+			if int(ARTICLE) == True:
+				print "U"
+			print "Error Insert Article Again... "
+			raw_input("press enter to continue")
+		except:
+			print "Article Added"
+			break
+	QUANTITY()
+def QUANTITY():
+	QUAN = True
+	while QUAN == True:
+		QUANTITY = raw_input("Insert Quantity article\n")
+		try:
+			int(QUANTITY)
+			PRICE()	
+			return False
+		except ValueError:
+			print "Only numbers"
+def PRICE():
+	PRI = True
+	while PRI == True:
+		PRICE = float(raw_input("Insert the price of the article\n"))
+		try:
+			float(PRICE)
+			QUESTION()
+			PRI = False
+		except ValueError:
+			print "Only numbers"
+def Sell():
+	SELL [ARTICLE] = QUANTITY, PRICE
+#Here we create a function that allow to the user an option to get out
+def EXIT():
 	print "Thanks for Visit Us"
-	raw_input("Press Enter for Continue n.n")
+	raw_input("Press Enter for Continue ...")
 	sys.exit()
-
-def MENU():#Here we create a menu that shows the options that user can select!
+#Here we create a menu that shows the options that user can select!
+def MENU():
 	while True: 
 		os.system("cls")
 		print "Welcome, please select an option\n"
 		print "#1 Add an item "
 		print "#2 Sell Articles"
 		print "#3 Exit"
-		DATA=raw_input("Insert an Option\n")
-		if DATA =="1":
-			INPUTS()  
-		#elif DATA == "2":
-		if DATA == "3":
+		DATA=input("Insert an Option\n")
+		if DATA ==1:
+			ARTICLE() 
+		if DATA == 2:
+			print SELL
+		elif DATA == 3:
 			EXIT()
 		else:
 			print "No entiendo la instruccion"
