7eca0f592ccfbd09d5d49c07cd7b1be11aaa2dc4
Javie1998 <floresjavier251@gmail.com>
2015-09-29 14:32:53 -0600

    Estetic

--- application.py
+++ application.py
@@ -11,20 +11,56 @@ import sys
 PRIC = {}
 INV = {}
 MULTY = 0
-total = 0
-def INVOICE():
-    pass
+TOTAL = 0
+#def INVOICE():
+    #pass
 
 def DONE():
-    total = 0
+    TOTAL = 0
     for i in PRIC:
-        MULTY = int(PRIC[i]) * int(INV[i])
+        MULTY = float(PRIC[i]) * float(INV[i])
         print "The total of "+ i + " is: " + str(MULTY) +"$\n"
-        total = MULTY + total
-    print "Invoice Total is: " + str(total)
-    total = total
-    raw_input("press enter")
-    DISCOUNT()
+        TOTAL = MULTY + TOTAL
+    #TOTAL = TOTAL
+    print "Invoice Total is: " + str(TOTAL)
+    raw_input("Press enter")
+    os.system("cls")
+
+    PREG = True
+    while PREG== True: 
+        try:
+            HAVE = raw_input("Do you have a card? y/n\n")
+            HAVE = HAVE.lower()
+            if HAVE == "y" or HAVE == "yes":
+                CARD = raw_input("Do you have a gold or silver card? \n")
+                CARD = CARD.lower()
+                try:
+                    if CARD == "gold" or CARD == "gold card":
+                        REDUCTION = TOTAL * 0.05
+                        TOTAL = TOTAL - REDUCTION
+                        print "Your total is :" + str(TOTAL)
+                        raw_input("Press enter")
+                        os.system("cls")
+                        PREG = False
+                    elif CARD == "silver" or CARD == "silver card":
+                        REDUCTION = TOTAL * 0.02
+                        TOTAL = TOTAL - REDUCTION
+                        print "Your total is :" + str(TOTAL)
+                        os.system("cls")
+                        raw_input("Press enter")
+                        PREG = False
+                    else: 
+                        print "This card doesn't exists"
+                except ValueError:
+                    print "insert a valid opction"
+                    PREG = True
+            elif HAVE == "n" or HAVE == "no":
+                print "Your total is :" + str(TOTAL)
+                os.system("cls")
+                raw_input("Press enter")
+        except ValueError:
+            print "Insert a valid option"
+            PREG = True
 
 def BEST_SELLER():
     for i in PRIC:
@@ -35,26 +71,15 @@ def BEST_SELLER():
             print "In the Store are " +str(INV[i])+ " "+ i + "s\n"
         elif INV[i] <= 1:
             print "There are " +str(INV[i])+ " "+ i + " In the Store\n"
-    OK = raw_input("Are you done to continue to the invoice? y/n\n")
+    OK = raw_input("...Are you done to continue to the invoice? y/n\n")
     OK = OK.lower()
+
     if OK == "y" or OK == "yes":
         DONE()
     elif OK == "n" or OK == "no":
         QUESTION()
-def DISCOUNT():
-    CARD = raw_input("Do you have a gold or silver card?\n")
-    CARD = CARD.lower()
-    if CARD == "gold" or CARD == "gold card":
-        REDUCTION = total * 0.5
-        total = total - REDUCTION
-        INVOICE()
-    elif CARD == "silver" or CARD == "silver card":
-        REDUCTION = total * 0.2
-        total = total - REDUCTION
-         
-        INVOICE()
-    else:
-        TOTAL= TOTAL   
+#def DISCOUNT():
+     
 #here the cashier can insert another articles
 def QUESTION():
     QUEST = raw_input("Do you want to Insert another article? Y/N or you're Done?\n")
@@ -96,7 +121,7 @@ def ARTICLES():
                 INV[ARTICLE]= QUANTITY
                 QUAN = False
             else:
-                print "***Fatala Error*** \n Only integer numbers!"
+                print "***Fatal Error*** \n Only integer numbers!"
                 QUAN = True
         except ValueError:
             print "Only numbers!!"
@@ -107,14 +132,14 @@ def ARTICLES():
             if float(PRICE):
                 PRIC[ARTICLE]= PRICE
                 os.system("cls")
-                QUESTION()
                 PRI = False
+                QUESTION()
             else:
                 print "***Fatal Error*** \n Only float numbers"
                 PRI = True
         except ValueError:
             print ">>>Only Float numbers<<<"
-            PRI = True
+            #PRI = True
 
 #Here we create a function that allow to the user an option to get out
 def EXIT():
