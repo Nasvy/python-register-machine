7b6935259c73fd2af22f9e881158021e202672ee
Javie1998 <floresjavier251@gmail.com>
2015-09-30 17:20:22 -0600

    Version pre-revision

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 """
->>>In this Application We create a register machine 
+>>>In this Application We create a register machine
 >>>when you use a Gold card you gonna recieve a discount of 5%
 or if you use a Silver card you gonna recieve a discount of 2%
 >>>
@@ -7,166 +7,184 @@ or if you use a Silver card you gonna recieve a discount of 2%
 print """
 """
 import os
-import sys 
+import sys
 PRIC = {}
 INV = {}
-MULTY = 0
-TOTAL = 0
-LIST=[]
-BILL={}
-#def INVOICE():
-    #pass
-
-def DONE():
-    PREG = True
-    while PREG == True:
+LIST = []
+BILL = {}
+def finish():
+    """Here is the final part of the program"""
+    print "When you're ready write 'Done'"
+    preg = True
+    while preg == True:
         for i in INV:
-            BUY = raw_input("Which article or articles do you want to buy?")
-            BUY = BUY.lower()
-            if BUY == BUY in INV and INV[i]>=0:
-                ADD = LIST.append(BUY)
+            buy = raw_input("Which article or articles do you want to buy?\n")
+            buy = buy.lower()
+            if buy == buy in INV and INV[i] >= 0:
+                LIST.append(buy)
                 print "In your shopping cart are: \n" + str(LIST)
-                VAR = LIST.count(i)
-                BILL[BUY]= VAR
-                PREG = True
-            elif BUY == "gold" or BUY == "gold card" or BUY == "silver" or BUY == "silver card":
-                ADD = LIST.append(BUY)
+                var = LIST.count(i)
+                BILL[buy] = var
+                preg = True
+            elif buy not in INV and (buy != "silver") and (buy != "gold") and (buy != "done"):
+                print "We don't have this item, insert a valid Item"
+                raw_input("Press Enter")
+                preg = True
+            elif buy == "gold" or buy == "gold card" or buy == "silver" or buy == "silver card":
+                LIST.append(buy)
                 print "In your shopping cart are: \n" + str(LIST)
-            elif BUY == "done":
-                TOTAL = 0
-                for i in PRIC:
-                    if i == i in LIST:
-                        MULTY = float(PRIC[i]) * float(BILL[i])
-                        print "The total of "+ i + " is: " + str(MULTY) +"$\n"
-                        TOTAL = MULTY + TOTAL
-                for i in LIST:
-                    if "gold" in LIST or "gold card" in LIST:
-                        print "Invoice Subtotal is: " "%.2f" %float(TOTAL)
-                        REDUCTION = TOTAL * 0.05
-                        TOTAL = TOTAL - REDUCTION
-                        print "Your Total with discount is: " "%.2f" %float(TOTAL)
-                        TOTAL = (TOTAL * 0.12) + TOTAL
-                        print "Your total is :" + "%.2f" % float(TOTAL)
-                        raw_input("Press enter")
-                        os.system("cls")
-                        RESET()
-                        MENU() 
-                    elif "silver" in LIST or "silver card" in LIST:
-                        REDUCTION = TOTAL * 0.02
-                        TOTAL = TOTAL - REDUCTION
-                        TOTAL = (TOTAL * 0.12) + TOTAL
-                        print "Your total is: " "%.2f" %float(TOTAL)
-                        raw_input("Press enter")
-                        os.system("cls")
-                        RESET()
-                        MENU()
-                    elif "silver" in LIST and "gold" in LIST:
-                        print "Invoice Total is: " "%.2f" %float(TOTAL)
-                        REDUCTION = TOTAL * 0.05
-                        TOTAL = TOTAL - REDUCTION
-                        TOTAL = (TOTAL * 0.12) + TOTAL
-                        print "Your total is: " + "%.2f" % float(TOTAL)
-                        raw_input("Press enter")
-                        os.system("cls")
-                        RESET()
-                        MENU()
-                    else:
-                        print "Invoice Total is: " "%.2f" %float(TOTAL)
-                        TOTAL = (TOTAL * 0.12) + TOTAL
-                        print "Your total is: "  "%.2f" % float(TOTAL)
-                        raw_input("Press enter")
-                        os.system("cls")
-                        RESET()
-                        MENU()
-        #raw_input ("Press enter")
-
-def RESET():
-    del LIST[0:]
-def BEST_SELLER():
+            elif buy == "done":
+                finaly()
+def finaly():
+    """here make the bill for the user"""
+    os.system("cls")
+    total = 0
     for i in PRIC:
-        print "Article: "+i
+        if i == i in BILL:
+            multy = float(PRIC[i]) * float(BILL[i])
+            print "The price of total of your "+ i + "(s) is: " + str(multy) +"$\n"
+            total = multy + total
+    for i in LIST:
+        if "gold" in LIST or "gold card" in LIST:
+            print "Invoice Subtotal is: ........" "%.2f" %float(total)
+            total = (total * 0.12) + total
+            print "Your total with IVA is: ....." "%.2f" % float(total)
+            reduction = total - (total * 0.05)
+            print "Your total with discount is: " "%.2f" %float(reduction)
+            print "_____________________________________"
+            raw_input("Press enter")
+            os.system("cls")
+            reset()
+            menu()
+        elif "silver" in LIST or "silver card" in LIST:
+            print "Invoice Subtotal is: ........" "%.2f" %float(total)
+            total = (total * 0.12) + total
+            print "Your total with IVA is: ....." "%.2f" %float(total)
+            reduction = total - (total * 0.02)
+            print "Your total with discount is: " "%.2f" %float(reduction)
+            print "_____________________________________"
+            raw_input("Press enter")
+            os.system("cls")
+            reset()
+            menu()
+        elif "silver" in LIST and "gold" in LIST:
+            print "Invoice Subtotal is: ........" "%.2f" %float(total)
+            reduction = total - (total * 0.05)
+            total = (total * 0.12) + total
+            print "Your total with IVA is: ....." "%.2f" % float(total)
+            print "_____________________________________"
+            total = total - reduction
+            print "Your total with discount is: " "%.2f" %float(reduction)
+            raw_input("Press enter")
+            os.system("cls")
+            reset()
+            menu()
+        else:
+            print "Invoice total is: ..........." "%.2f" %float(total)
+            total = (total * 0.12) + total
+            print "Your total with IVA is ......" "%.2f" % float(total)
+            raw_input("Press enter")
+            os.system("cls")
+            reset()
+            menu()
+def reset():
+    """This function reset the list of articles"""
+    del LIST[0:]
+def best_seller():
+    """This Function ask the user what does he wants to buy"""
+    for i in PRIC:
+        print "article: "+i
         print "The cost is "+str(PRIC[i])
-    OK = raw_input("...Are you done to continue to the invoice? y/n\n")
-    OK = OK.lower()
-
-    if OK == "y" or OK == "yes":
-        DONE()
-        os.system("cls")
-    elif OK == "n" or OK == "no":
-        QUESTION()
-#def DISCOUNT():
-     
+    invoice = True
+    try:
+        while invoice == True:
+            yes = raw_input("...Are you Done to continue to the invoice? y/n\n")
+            yes = yes.lower()
+            if yes == "y" or yes == "yes":
+                finish()
+                os.system("cls")
+            elif yes == "n" or yes == "no":
+                question()
+            else:
+                print "i dont understand"
+                invoice = True
+    except ValueError:
+        print "Insert a valid option"
 #here the cashier can insert another articles
-def QUESTION():
-    QUEST = raw_input("Do you want to Insert another article? Y/N\n")
-    QUEST = QUEST.lower()
-    if QUEST == "y" or QUEST == "yes":
+def question():
+    """This function ask the user if he wants to continue adding articles"""
+    quest = raw_input("Do you want to Insert another article? Y/N\n")
+    quest = quest.lower()
+    if quest == "y" or quest == "yes":
         os.system("cls")
-        ARTICLES()
-    elif QUEST == "n" or QUEST == "no":
-        MENU()
+        articles()
+    elif quest == "n" or quest == "no":
+        menu()
     else:
         print "Insert a valid option"
-        QUESTION()
+        question()
 #Here the Cashier insert the articles
-def ARTICLES():
-    ART = True
-    ASD = False
-    while ASD == False:
-        ARTICLE = raw_input(">>>Insert an article<<<\n")
-        ARTICLE = ARTICLE.lower()
+def articles():
+    """Here the User insert the articles and the prices"""
+    asd = False
+    while asd == False:
+        article = raw_input(">>>Insert an article<<<\n")
+        article = article.lower()
         try:
-            if  str(ARTICLE).isalpha() == True:
-                print "Article Added"
-                INV[ARTICLE]=""
-                PRIC[ARTICLE]=""
-                ASD = True
+            if article == "done":
+                print "You can't write done here!"
+                question()
+                os.system()
+            if  str(article).isalpha() == True:
+                print "article Added"
+                INV[article] = ""
+                PRIC[article] = ""
+                asd = True
             else:
                 print "Error"
-                ASD = False
+                asd = False
                 raw_input(">>>press enter to continue<<<")
         except ValueError:
-            print "***Fatal Error*** \n Insert Article Again***"
+            print "***Fatal Error*** \n Insert article Again***"
 
-    PRI = True
-    while PRI == True:
-        PRICE = raw_input(">>>Insert the price of the article<<<\n")
+    pri = True
+    while pri == True:
+        price = raw_input(">>>Insert the price of the article<<<\n")
         try:
-            if float(PRICE):
-                PRIC[ARTICLE]= PRICE
+            if float(price):
+                PRIC[article] = price
                 os.system("cls")
-                PRI = False
-                QUESTION()
+                pri = False
+                question()
             else:
                 print "***Fatal Error*** \n Only float numbers"
-                PRI = True
+                pri = True
         except ValueError:
             print ">>>Only Float numbers<<<"
-            #PRI = True
-
 #Here we create a function that allow to the user an option to get out
-def EXIT():
+def out():
+    """This function exits the program"""
     print "Thanks for Visit Us."
     raw_input(">>>Press Enter for Continue<<<")
     sys.exit()
-#Here we create a menu that shows the options that user can select!
-def MENU():
-    while True: 
+def menu():
+    """Here we create a menu that shows the options that user can select!"""
+    while True:
         os.system("cls")
         print "Welcome, please select an option\n"
         print "#1 Add an item "
-        print "#2 Sell Articles"
-        print "#3 Exit"
-        DATA=raw_input(">>>Select an Option<<<\n")
-        if DATA =="1":
-            ARTICLES() 
-        elif DATA == "2":
-            BEST_SELLER()
-        elif DATA == "3":
-            EXIT()
+        print "#2 Sell articles"
+        print "#3 exit"
+        data = raw_input(">>>Select an Option<<<\n")
+        if data == "1":
+            articles()
+        elif data == "2":
+            best_seller()
+        elif data == "3":
+            out()
         else:
             print "***Fatal Error***\n I don't understand the Instruction"
             raw_input("press enter to continue")
             os.system("cls")
             os.system("clear")
-MENU()
+menu()
