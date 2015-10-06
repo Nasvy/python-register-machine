eff28f2a4c2c1601ef2f9ffc937cdbfb067e28a1
Nasvy <nasvyhg@gmail.com>
2015-10-06 13:16:38 -0600

    fixed

--- application.py
+++ application.py
@@ -1,191 +1,170 @@
-"""
->>>In this Application We create a register machine
->>>when you use a Gold card you gonna recieve a discount of 5%
-or if you use a Silver card you gonna recieve a discount of 2%
->>>
-"""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 import sys
-PRIC = {}
-INV = {}
-LIST = []
-BILL = {}
-COMPRAS = []
-def finish():
-    """Here is the final part of the program"""
-    print "When you're ready write 'Done'"
-    preg = True
-    while preg == True:
-        for i in INV:
-            buy = raw_input("Which article or articles do you want to buy?\n")
-            buy = buy.lower()
-            if buy == buy in INV:
-                LIST.append(buy)
-                print "In your shopping cart are: \n" + str(LIST)
-                BILL[buy] = LIST.count(i)
-                preg = True
-            elif buy not in INV and (buy != "silver") and (buy != "gold") and (buy != "done"):
-                print "We don't have this item, insert a valid Item"
-                raw_input("Press Enter")
-                preg = True
-            elif buy == "gold" or buy == "gold card" or buy == "silver" or buy == "silver card":
-                LIST.append(buy)
-                print "In your shopping cart are: \n" + str(LIST)
-            elif buy == "done":
-                finaly()
-def finaly():
-    """here make the bill for the user"""
-    os.system("cls")
-    total = 0
-    for i in PRIC:
-        multy = float(LIST.count(i)) * float(PRIC[i])
-        unit = LIST.count(i)
-        print "Price of your " + str(unit) +" "+ i + "(s) is: ""$.""%.2f" %float(multy) +"\n"
-        total = multy + total
-    cards(total)
+from collections import OrderedDict
+#from email.MIMEMultipart import MIMEMultipart
+#from email.MIMEText import MIMEText
 
+CAP_AND_COUN = {}
+CAPITALS = []
+COUNTRIES = []
+ORDERED_LIST = {}
+a = "Countries"
+b = "Capitals"
+import smtplib, getpass
 
-def cards(total):
-    """ Here is the cards functions """
-    while True:
-        if "gold" in LIST or "gold card" in LIST:
-            print "Invoice Subtotal is: ..................." "$" "%.2f" %float(total)
-            print "The tax of IVA is:......................" "$" "%.2f" %float(total * 0.12)
-            total = (total * 0.12) + total
-            print "Your total with IVA is: ................" "$" "%.2f" %float(total)
-            reduction = total - (total * 0.05)
-            print "Our discount with gold card is:........." "$" "%.2f" %float(total*0.05)
-            print "___________________________________________"
-            print "Your total with discount is:............" "$" "%.2f" %float(reduction)
-        elif "silver" in LIST or "silver card" in LIST:
-            print "Invoice Subtotal is: ..................." "$" "%.2f" %float(total)
-            print "The tax of IVA is:......................" "$" "%.2f" %float(total * 0.12)
-            total = (total * 0.12) + total
-            print "Your total with IVA is: ................" "$" "%.2f" %float(total)
-            reduction = total - (total * 0.02)
-            print "Our discount with silver card is:......." "$" "%.2f" %float(total*0.02)
-            print "___________________________________________"
-            print "Your total with discount is:............" "$" "%.2f" %float(reduction)
-        elif "silver" in LIST and "gold" in LIST:
-            print "Invoice Subtotal is: ..................." "$" "%.2f" %float(total)
-            print "The tax of IVA is:......................" "$" "%.2f" %float(total * 0.12)
-            total = (total * 0.12) + total
-            print "Your total with IVA is: ................" "$" "%.2f" %float(total)
-            reduction = total - (total * 0.05)
-            print "Our discount with gold card is:........." "$" "%.2f" %float(total*0.05)
-            print "___________________________________________"
-            print "Your total with discount is:............" "$" "%.2f" %float(reduction)
-        else:
-            print "Invoice total is: ......................" "$" "%.2f" %float(total)
-            print "The tax of IVA is:......................" "$" "%.2f" %float(total * 0.12)
-            total = (total * 0.12) + total
-            print "Your total with IVA is ................." "$" "%.2f" %float(total)
-            print "You don't have a discount card.........." "$" "%.2f" %float(0)
-            print "___________________________________________"
-            print "The total of your invoice is:..........." "$" "%.2f" %float(total)
+def main():
+    print "Send email by gmail"
 
-        raw_input("Press enter")
-        os.system("cls")
-        reset()
-        menu()
+    fromaddr = raw_input("Count from gmail: ")
+    password = getpass.getpass("Password: ")
+    toaddrs = raw_input("to: ")
+    body = ""
+    for country, capital in ORDERED_LIST.iteritems():
+        body = body + country + "-" + capital + "\n"
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr #This saves the mail of the sender
+    msg['To'] = toaddrs  #This saves the mail of the receiver
+    msg['Subject'] = "Countres and Capitals"  #This saves the subject
+    msg.attach(MIMEText(body, 'plain')) #This saves the message
 
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(username,password)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddrs, text)
+        server.quit()
+        print "yes"
+    except:
+        print "ups"
 
-def reset():
-    """This function reset the list of articles"""
-    del LIST[0:]
-def best_seller():
-    """This Function ask the user what does he wants to buy"""
-    for i in PRIC:
-        print "article: "+i
-        print "The cost is " "$" "%.2f" %float(PRIC[i])
-    finish()
-def question():
-    """This function ask the user if he wants to continue adding articles"""
-    quest = raw_input("Do you want to Insert another article? Y/N\n")
-    quest = quest.lower()
-    if quest == "y" or quest == "yes":
-        os.system("cls")
-        articles()
-    elif quest == "n" or quest == "no":
-        menu()
+def ORDER():
+    """This function order the capitals"""
+    LIMPIAR()
+    ordered = OrderedDict(sorted(CAP_AND_COUN.items(), key=lambda x: x[1:]))
+    print a.center(20,"="), b.center(20,"=")
+    for k , v in ordered.items():
+        print k.center(20), v.center(20)
+        ORDERED_LIST[k]=v
+    raw_input("press enter")
+    MENU()
+def CAPITALS_AND_COUNTRIES():
+    """Here shows the capitals and countries"""
+    print a.center(20,"="), b.center(20,"=")
+    for i in CAP_AND_COUN:
+        print i.center(20), CAP_AND_COUN[i].center(20)
+    raw_input("Press enter")
+    LIMPIAR()
+    MENU()
+def LIST_CAPITAL():
+    """Here shows the list of the capitals"""
+    print b.center(20,"=")
+    for i in CAPITALS:
+        print i.center(20)
+    raw_input("Press enter")
+    LIMPIAR()
+    MENU()
+def LIST_COUNTRIES():
+    """Here Shows the list of the Countries"""
+    print a.center(20,"=")
+    for i in COUNTRIES:
+        print i.center(20)
+    raw_input("Press enter")
+    LIMPIAR()
+    MENU()
+def QUEST():
+    """This function ask the user if he wants to insert another Country and Capital"""
+    ans = raw_input("Do you want to insert another Country? y/n\n")
+    ans = ans.lower()
+    if ans == "y" or ans == "yes":
+        INSERT_COUNTRIES()
+    elif ans == "n" or ans == "no":
+        LIMPIAR()
+        MENU()
     else:
+        print "Choose a correct option"
+        raw_input("Press enter")
+        LIMPIAR()
+        QUEST()
+def INSERT_COUNTRIES():
+    """Here the user insert the Country and the Capital"""
+    try:
+        coun = True
+        while coun == True:
+            country = raw_input("Insert a Country\n")
+            country = country.title()
+            for c in country:
+                if c.isdigit() == False:
+                    coun = False
+                else:
+                    print "write only words n.n"
+                    raw_input("Press enter")
+                    coun = True
+                    LIMPIAR()
+                    break
+        COUNTRIES.append(country)
+        cap = True
+        while cap == True:
+            capital = raw_input("Insert a Capital\n")
+            capital = capital.title()
+            for c in capital:
+                if c.isdigit() == False:
+                    cap = False
+                else:
+                    print "write only words n.n"
+                    raw_input("Press enter")
+                    cap = True
+                    LIMPIAR()
+                    break
+        CAPITALS.append(capital)
+    except ValueError:
         print "Insert a valid option"
-        question()
-def articles():
-    """Here the User insert the articles and the prices"""
-    asd = False
-    while asd == False:
-        article = raw_input(">>>Insert an article<<<\n")
-        article = article.lower()
-        try:
-            if article == "done":
-                print "You can't write done here!"
-                question()
-                os.system()
-            if  str(article).isalpha() == True:
-                print "article Added"
-                INV[article] = ""
-                PRIC[article] = ""
-                asd = True
-            else:
-                print "Error you can only insert words"
-                asd = False
-                raw_input(">>>press enter to continue<<<")
-        except ValueError:
-            print "***Fatal Error*** \n Insert article Again***"
-
-    pri = True
-    while pri == True:
-        price = raw_input(">>>Insert the price of the article<<<\n")
-        try:
-            if float(price):
-                PRIC[article] = price
-                os.system("cls")
-                pri = False
-                question()
-            else:
-                print "***Fatal Error*** \n Only float numbers"
-                pri = True
-        except ValueError:
-            print ">>>Only Float numbers<<<"
-def out():
-    """This function exits the program"""
-    print "Thanks for Visit Us."
-    raw_input(">>>Press Enter for Continue<<<")
+    CAP_AND_COUN[country] = capital
+    print CAP_AND_COUN
+    LIMPIAR()
+    QUEST()
+def LIMPIAR():
+    """This function cleans the screen"""
+    os.system("cls")
+    os.system("clear")
+def OUT():
+    """This function exit the program"""
     sys.exit()
-def menu():
-    """Here we create a menu that shows the options that user can select!"""
-    while True:
-        os.system("cls")
-        print u""" /$$      /$$                  /$$                 /$$        """
-        print u"""| $$$    /$$$                 | $$                | $$        """
-        print u"""| $$$$  /$$$$ /$$$$$$  /$$$$$$| $$   /$$ /$$$$$$ /$$$$$$       """
-        print u"""| $$ $$/$$ $$|____  $$/$$__  $| $$  /$$//$$__  $|_  $$_/       """
-        print u"""| $$  $$$| $$ /$$$$$$| $$  \__| $$$$$$/| $$$$$$$$ | $$  __   """
-        print u"""| $$\  $ | $$/$$__  $| $$     | $$_  $$| $$_____/ | $$ /$$           """
-        print u"""| $$ \/  | $|  $$$$$$| $$     | $$ \  $|  $$$$$$$ |  $$$$/      """
-        print u"""|__/     |__/\_______|__/     |__/  \__/\_______/  \___/      """
-
-        print"""========================================="""
-        print"""|----------------Welcome...-------------|"""
-        print"""|-------Hi, here you can insert---------|"""
-        print"""|-----All the products you want---------|"""
-        print"""========================================="""
-
-        print "Welcome, please select an option\n"
-        print "#1 Add an item "
-        print "#2 Sell articles"
-        print "#3 exit"
-        data = raw_input(">>>Select an Option<<<\n")
-        if data == "1":
-            articles()
-        elif data == "2":
-            best_seller()
-        elif data == "3":
-            out()
-        else:
-            print "***Fatal Error***\n I don't understand the Instruction"
-            raw_input("press enter to continue")
-            os.system("cls")
-            os.system("clear")
-menu()
+def MENU():
+    """This is the menu that the user watch"""
+    LIMPIAR()
+    print "Welcome to Captials and Countries".center(40,"=")
+    print "-----1. Insert a country----------------"
+    print "-----2. Countries list------------------"
+    print "-----3. Capital list--------------------"
+    print "-----4. Countries and Capitals----------"
+    print "-----5. Countries and capitals by order-"
+    print "-----6. All by mail---------------------"
+    print "-----7. Exit----------------------------"
+    men = raw_input("Choose an option:\n")
+    men = men.lower()
+    if men == "1" or men == "country":
+        LIMPIAR()
+        INSERT_COUNTRIES()
+    elif men == "2" or men == "countries":
+        LIMPIAR()
+        LIST_COUNTRIES()
+    elif men == "3" or men == "capitals":
+        LIMPIAR()
+        LIST_CAPITAL()
+    elif men == "4" or men == "all":
+        LIMPIAR()
+        CAPITALS_AND_COUNTRIES()
+    elif men == "5" or men =="allordered":
+        ORDER()
+    elif men == "6" or men == "allmail":
+        main()
+    elif men == "7":
+        OUT()
+    else:
+        print "Choose a correct option please"
+        raw_input("Press enter")
+        MENU()
+MENU()
